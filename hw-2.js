2709d056900c5b579fab94842c82a4be8fc4165b
tatianko <kolchtatiana@loniir.ru>
2025-09-22 01:46:24 +0300

    Добавлена домашка по js

---
+++ hw-2.js
@@ -0,0 +1,61 @@
+/* Задание 1 */
+let a = 10;
+alert(a);
+
+a = 20;
+alert(a);
+
+/* Задание 2 */
+let firstIPhoneReleaseYear = 2007;
+alert(`Год выпуска первого iPhone ${firstIPhoneReleaseYear}`);
+
+/* Задание 3 */
+let javaScriptCreatorName = "Брендан Эйх";
+alert(`Имя создателя языка JavaScript ${javaScriptCreatorName}`);
+
+/* Задание 4 */
+let var1 = 10;
+let var2 = 2;
+
+alert(`${var1} + ${var2} = ${var1 + var2}`);
+alert(`${var1} - ${var2} = ${var1 - var2}`);
+alert(`${var1} * ${var2} = ${var1 * var2}`);
+alert(`${var1} / ${var2} = ${var1 / var2}`);
+
+/* Задание 5 */
+let var3 = 2;
+let result = var3 ** 5;
+alert(`${var3} в 5-ой степени ${result}`);
+
+/* Задание 6 */
+a = 9;
+let b = 2;
+result = a % b;
+
+alert(`Остаток от деления ${a} на ${b} равняется ${result}`);
+
+/* Задание 7 */
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num += 1;
+num -= 1;
+alert(num);
+
+/* Задание 8 */
+let age = Number(prompt("Сколько вам лет?"));
+
+alert(`Ваш возраст ${age}`);
+
+/* Задание 9 */
+const user = {
+    name: 'Alex',
+    age: 20,
+    isAdmin: false
+};
+
+/* Задание 10 */
+let userName = String(prompt("Ваше имя"));
+alert(`Привет, ${userName}`);
