ff3ee878870c4437b855c126b91a8cf396946fa6
tatianko <kolchtatiana@loniir.ru>
2025-09-30 00:00:05 +0300

    Домашка N9, игра 6, анимированная полоса

--- animatedLine.js
+++ animatedLine.js
@@ -1,22 +1,39 @@
 /**
- * Генератор случайных цветов
- * @returns {`#${string}`} случайный цвет в hex
+ * Анимированная полоса с названиями игр
  */
-const generateRandomColor = () => {
-    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
 
-/**
- * Старт игры
- */
-let game6 = () => {
-    const gamesEl = document.querySelector(".games");
-    const aboutEl = document.querySelector(".about");
+// список всех заголовков описания игр
+const gameTitleList =
+    document.querySelectorAll(".about-game__title");
 
-    if (gamesEl && aboutEl) {
-        const backgroundColor = generateRandomColor();
+// блок с анимированной полосой
+const animatedLine = document.querySelector(".animated-line-wrapper");
 
-        gamesEl.style.backgroundColor = backgroundColor;
-        aboutEl.style.backgroundColor = backgroundColor;
+if (gameTitleList && animatedLine) {
+    // массив названий игр
+    const gameTitleStrings = [];
+
+    gameTitleList.forEach(item => {
+        if (item.textContent.startsWith('Простая в')) {
+            // простая викторина превратилась в викторину
+            gameTitleStrings.push(item.textContent
+                .split(' ')
+                .pop());
+        } else {
+            gameTitleStrings.push(item.textContent);
+        }
+    });
+
+    // восстанавливаем порядок названий с макета
+    gameTitleStrings.sort((a, b) => b.localeCompare(a));
+    gameTitleStrings.unshift(gameTitleStrings.pop());
+
+    // добавление элементов с названием игры на анимированную полосу
+    for (let i = 0; i < gameTitleStrings.length; i++) {
+        const newEl = document.createElement('p');
+        newEl.textContent = gameTitleStrings[i];
+        newEl.className = 'animated-line__text';
+
+        animatedLine.append(newEl);
     }
 }
